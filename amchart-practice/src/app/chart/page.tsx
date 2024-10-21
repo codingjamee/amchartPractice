026cd5d9f6f90dfd7cbbaeec0385e73d811a3fb4
허제인 <codingjamee@gmail.com>
2024-10-21 22:56:18 +0900

    #add amchart

---
+++ amchart-practice/src/app/chart/page.tsx
@@ -0,0 +1,12 @@
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+
+const ChartPage = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center h-100">
+      ChartPage
+    </div>
+  );
+};
+
+export default ChartPage;
